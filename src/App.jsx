bc1ddfe04657dd07130fef83e3360d6addc0b3ce
Karan <email@karanshergill.com>
2024-09-03 01:03:32 +0530

    fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import InsertLabelForm from './components/InsertForm.jsx';
+import { Separator } from "@/components/ui/separator"
+import './App.css';
 
 const App = () => {
   const handleFormSubmit = async (data) => {
@@ -23,9 +25,17 @@ const App = () => {
   };
 
   return (
-    <div className='bg-black text-white h-screen'>
-      <h1>Insert Label</h1>
-      <InsertLabelForm onSubmit={handleFormSubmit} />
+    <div className='flex justify-center p-8 bg-black text-white h-screen'>
+      <main className='w-5/6'>
+        <h1 className='text-3xl font-display font-semibold text-center'>DBMS Dashboard</h1>
+        <div className='my-4'>
+        <Separator/>
+        </div>
+        <div className='flex flex-col'>
+          <h3 className='text-lg font-display'>Create Record</h3>
+          <InsertLabelForm onSubmit={handleFormSubmit} />
+        </div>
+      </main>
     </div>
   );
 };
--- src/components/InsertForm.jsx
+++ src/components/InsertForm.jsx
@@ -1,8 +1,22 @@
 import PropTypes from 'prop-types';
 import { useForm } from 'react-hook-form';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectLabel,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+
+
 
 const InsertRecordsForm = ({ onSubmit }) => {
-    const { register, handleSubmit, reset } = useForm();
+    const { register, handleSubmit, reset, setValue, watch } = useForm();
 
     const handleFormSubmit = async (data) => {
         await onSubmit(data);
@@ -10,22 +24,34 @@ const InsertRecordsForm = ({ onSubmit }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit(handleFormSubmit)}>
-            <input
+        <form onSubmit={handleSubmit(handleFormSubmit)} className='flex flex-col font-body w-max gap-2'>
+            <Input
                 {...register('label_val', { required: true })}
                 type="text"
-                placeholder="Enter label value"
+                placeholder="Enter value"
             />
-            
-            <select className='text-gray-500' {...register('table_name', { required: true })} defaultValue="">
-                <option value="" disabled hidden>
-                    Select a table
-                </option>
-                <option value="megadns">Mega DNS</option>
-                <option value="doddns">DOD DNS</option>
-                <option value="dotfiles">DOT Files</option>
-            </select>
-            <button type="submit">Insert</button>
+
+            <Select className='text-gray-500' {...register('table_name', { required: true })} defaultValue="">
+                <SelectTrigger>
+                    <SelectValue placeholder="Select a table" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectLabel>DNS</SelectLabel>
+                        <SelectItem value="megadns">Mega DNS</SelectItem>
+                        <SelectItem value="doddns">DOD DNS</SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                        <SelectLabel>Directories</SelectLabel>
+                        <SelectItem value="directories">Common Directories</SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                        <SelectLabel>Files</SelectLabel>
+                        <SelectItem value="dotfiles">Dot Files</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+            <Button type="submit">Insert</Button>
         </form>
     );
 };
